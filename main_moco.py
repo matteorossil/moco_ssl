d9647cdb3c1f9b54d2737c779c5df75721b0dbb5
mrossigrad <mrossigrad@gmail.com>
2023-10-09 12:45:25 -0400

    test

--- main_moco.py
+++ main_moco.py
@@ -248,6 +248,9 @@ def main_worker(gpu, ngpus_per_node, args):
             # For multiprocessing distributed training, rank needs to be the
             # global rank among all the processes
             args.rank = args.rank * ngpus_per_node + gpu
+
+        print("rank", args.rank)
+
         dist.init_process_group(
             backend=args.dist_backend,
             init_method=args.dist_url,
